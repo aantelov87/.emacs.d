06f4feeb681cd82be56e70101297b2a46d60ee1c
Antonio Antelo <aantelov87@gmail.com>
2018-11-03 18:52:33 +0100

    add godef for jumping to symbols

--- go_tools.sh
+++ go_tools.sh
@@ -3,5 +3,6 @@
 go get -u github.com/mdempsky/gocode
 go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/tools/cmd/gorename
+go get -u github.com/rogpeppe/godef
 go get -u golang.org/x/tools/cmd/guru
 go get -u github.com/kisielk/errcheck
